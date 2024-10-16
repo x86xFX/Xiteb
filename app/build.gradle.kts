8ad0e24f86a8f3a9e0647eba92d8fe536f96a827
theek <theek1010@gmail.com>
2024-10-16 12:39:31 +0530

    CleanedUp Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,9 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isDebuggable = false
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
